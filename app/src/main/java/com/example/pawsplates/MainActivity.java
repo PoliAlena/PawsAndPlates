715f23daf4edbe8c3f759ca5f7f5f73990463d29
PoliAlena <128645010+PoliAlena@users.noreply.github.com>
2024-03-19 11:19:00 +0300

    ff

---
+++ app/src/main/java/com/example/pawsplates/MainActivity.java
@@ -0,0 +1,21 @@
+package com.example.pawsplates;
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.View;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.aut);
+    }
+
+    public void EnterB(View view) {
+
+    }
+
+    public void RegB(View view) {
+    }
+}
